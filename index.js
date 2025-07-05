6f601bc22c9052ee09d28c2077034a0df9551b75
kaif HTML <kaifalikhan0090@gmail.com>
2025-07-05 09:16:07 +0530

    add some data link net

--- index.js
+++ index.js
@@ -27,9 +27,10 @@ mongoose
 
 // === Middlewares ===
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://instaclone0090.netlify.app"],
   credentials: true,
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
